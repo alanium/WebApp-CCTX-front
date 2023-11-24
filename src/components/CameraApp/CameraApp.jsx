318313005f11137a4a58dd2a96a2aebd4f763b0f
patricio <romanopatricioezequiel@gmail.com>
2023-11-24 09:27:01 -0300

    fix: asd

--- src/components/CameraApp/CameraApp.jsx
+++ src/components/CameraApp/CameraApp.jsx
@@ -3,10 +3,11 @@ import React, { useState, useRef, useEffect } from "react";
 import { FaCircle } from "react-icons/fa";
 import { MdChangeCircle } from "react-icons/md";
 import { BiSolidXCircle } from "react-icons/bi";
-import { initializeApp } from 'firebase/app';
-import 'firebase/storage';
+import { initializeApp } from "firebase/app";
+import "firebase/storage";
 import "./CameraApp.css";
 import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+import { useNavigate } from "react-router-dom";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAcCBS9ovlwg_Lg_yGKPILSsc_ETBb3_eE",
@@ -15,7 +16,7 @@ const firebaseConfig = {
   storageBucket: "fb-storage-49d33.appspot.com",
   messagingSenderId: "327363193304",
   appId: "1:327363193304:web:88b1cf55edee2322b6eaad",
-  measurementId: "G-2J1T6GN9R0"
+  measurementId: "G-2J1T6GN9R0",
 };
 
 initializeApp(firebaseConfig);
@@ -23,11 +24,12 @@ initializeApp(firebaseConfig);
 const CameraApp = (props) => {
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
+  const navigate = useNavigate();
   const [mediaStream, setMediaStream] = useState(null);
-  const [facingMode, setFacingMode] = useState('user');
+  const [facingMode, setFacingMode] = useState("user");
   const [location, setLocation] = useState(null);
-  const [url, setUrl] = useState([])
-
+  const [permissions, setPermissions] = useState(false);
+  const [url, setUrl] = useState([]);
 
   useEffect(() => {
     // Solicitar acceso a la ubicación cuando se monta el componente
@@ -43,10 +45,10 @@ const CameraApp = (props) => {
     // Iniciar la cámara y solicitar acceso a la ubicación
     const startCameraAndRequestLocation = async () => {
       await startCamera();
-      requestLocationAccess();
+      await requestLocationAccess();
     };
 
-    startCameraAndRequestLocation();
+    startCameraAndRequestLocation().then(setPermissions(true));
 
     return () => stopCamera();
   }, []);
@@ -65,7 +67,7 @@ const CameraApp = (props) => {
         videoRef.current.srcObject = stream;
       }
     } catch (error) {
-      console.error('Error accessing camera:', error);
+      console.error("Error accessing camera:", error);
     }
   };
 
@@ -75,13 +77,13 @@ const CameraApp = (props) => {
 
     try {
       await uploadBytes(imageRef, blob).then((snapshot) => {
-        console.log("uploaded a blob")
+        console.log("uploaded a blob");
       });
       const imageUrl = await getDownloadURL(ref(storage, `${imageRef}`));
-      setUrl([...url, imageUrl])
-      console.log('Image uploaded to Firebase:', imageUrl);
+      setUrl([...url, imageUrl]);
+      console.log("Image uploaded to Firebase:", imageUrl);
     } catch (error) {
-      console.error('Error uploading image to Firebase:', error);
+      console.error("Error uploading image to Firebase:", error);
     }
   };
 
@@ -111,7 +113,7 @@ const CameraApp = (props) => {
 
   const takePhoto = () => {
     if (videoRef.current && canvasRef.current) {
-      getLocation()
+      getLocation();
       const video = videoRef.current;
       const canvas = canvasRef.current;
 
@@ -119,75 +121,102 @@ const CameraApp = (props) => {
       canvas.width = video.videoWidth;
       canvas.height = video.videoHeight;
 
-      const context = canvas.getContext('2d');
+      const context = canvas.getContext("2d");
       context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-      canvas.toBlob((blob) => {
-        uploadImageToFirebase(blob);
-      }, 'image/png', 1);
+      canvas.toBlob(
+        (blob) => {
+          uploadImageToFirebase(blob);
+        },
+        "image/png",
+        1
+      );
 
-      props.enviarDatos({location: location, image: url}, "camera")
+      props.enviarDatos({ location: location, image: url }, "camera");
     }
   };
 
   const handleCanvasClick = (event) => {
     if (mediaStream) {
       const video = videoRef.current;
-  
+
       const { offsetX, offsetY } = event.nativeEvent;
       const clickX = offsetX / video.clientWidth;
       const clickY = offsetY / video.clientHeight;
-  
+
       const track = mediaStream.getVideoTracks()[0];
-      if (track && 'applyConstraints' in track) {
+      if (track && "applyConstraints" in track) {
         // Check if applyConstraints is supported
-        track.applyConstraints({
-          advanced: [{ focusPointOfInterest: { x: clickX, y: clickY } }],
-        })
-          .then(() => console.log('Camera focused successfully'))
-          .catch((error) => console.error('Error focusing camera:', error));
+        track
+          .applyConstraints({
+            advanced: [{ focusPointOfInterest: { x: clickX, y: clickY } }],
+          })
+          .then(() => console.log("Camera focused successfully"))
+          .catch((error) => console.error("Error focusing camera:", error));
       } else {
-        console.log('Applying constraints not supported on this device');
+        console.log("Applying constraints not supported on this device");
       }
     }
   };
 
   const switchCamera = () => {
-    setFacingMode((prevFacingMode) => (prevFacingMode === 'user' ? 'environment' : 'user'));
+    setFacingMode((prevFacingMode) =>
+      prevFacingMode === "user" ? "environment" : "user"
+    );
     stopCamera();
     startCamera();
   };
 
-  
-
   return (
     <div className="camera-container">
-      <video ref={videoRef} autoPlay playsInline className="video-preview" />
+      {permissions ? (
+        <div>
+          <video
+            ref={videoRef}
+            autoPlay
+            playsInline
+            className="video-preview"
+          />
 
-      <div className="canvas-container">
-        <canvas
-          ref={canvasRef}
-          className="hidden-canvas"
-          onClick={handleCanvasClick}
-        />
-      </div>
+          <div className="canvas-container">
+            <canvas
+              ref={canvasRef}
+              className="hidden-canvas"
+              onClick={handleCanvasClick}
+            />
+          </div>
 
-      <div className="button-container">
-        <button style={{ fontSize: "77px", color: "white" }} onClick={stopCamera}><BiSolidXCircle /></button>
-        <button
-          style={{ fontSize: "66px", color: "white" }}
-          onClick={takePhoto}
-        >
-          <FaCircle />{" "}
-        </button>
-        <button style={{ fontSize: "77px", color: "white" }} onClick={switchCamera}>
-          <MdChangeCircle />
-        </button>
-      </div>
+          <div className="button-container">
+            <button
+              style={{ fontSize: "77px", color: "white" }}
+              onClick={stopCamera}
+            >
+              <BiSolidXCircle />
+            </button>
+            <button
+              style={{ fontSize: "66px", color: "white" }}
+              onClick={takePhoto}
+            >
+              <FaCircle />{" "}
+            </button>
+            <button
+              style={{ fontSize: "77px", color: "white" }}
+              onClick={switchCamera}
+            >
+              <MdChangeCircle />
+            </button>
+          </div>
 
-      <div className="canvas-container">
-        <canvas ref={canvasRef} className="hidden-canvas" />
-      </div>
+          <div className="canvas-container">
+            <canvas ref={canvasRef} className="hidden-canvas" />
+          </div>
+        </div>
+      ) : (
+        <div className="global-containter">
+          <label>Grant location and camera permissions to use this component</label>
+          <button onClick={() => navigate("/")}>Go Back</button>
+        </div>
+      )}
     </div>
   );
 };
