3e059dbef9860e0566dff48334b1144062fd9e6e
patricio <romanopatricioezequiel@gmail.com>
2023-11-29 14:22:22 -0300

    fix: asd

--- src/components/CameraApp/NoMatches.jsx
+++ src/components/CameraApp/NoMatches.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-export default function NoMatches (event) {
+export default function NoMatches (props) {
 
     const navigate = useNavigate()
 
